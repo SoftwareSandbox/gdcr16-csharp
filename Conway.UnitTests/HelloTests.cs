b53ebfa6cacdaa62fedea3518360369ffac2718d
Wouter Groeneveld <wouter.groeneveld@gmail.com>
2016-12-12 16:46:26 +0100

    initial commit

---
+++ Conway.UnitTests/HelloTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Conway.UnitTests
+{
+    [TestFixture]
+    public class HelloTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
